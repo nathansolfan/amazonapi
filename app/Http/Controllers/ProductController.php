3dcf770d0ee35cb6e4c3731e2d9ad32c7fa2c9ed
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-07-10 23:54:58 +0100

    Product Controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PriceHistory;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    //
+    public function store(Request $request)
+    {
+        $product = Product::create($request->all());
+
+        // create initial price
+        PriceHistory::create([
+            'product_id' => $product->id,
+            'price' => $product->price,
+            'recorded_at' => now(),
+        ]);
+
+        return response()->json($product, 201);  
+    }
+
+
+    public function updatePrice(Request $request, $id)
+    {
+        $product = Product::findOrFail($id);
+        $product->update(['price' => $request->price]);
+
+        // record price change in price histories
+        PriceHistory::create([
+            'product_id' => $product->id,
+            'price' => $request->price,
+            'recorded_at' => now(),
+        ]);
+    }
+}

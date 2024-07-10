8a910e5e367fcd95f3be5ee9b2df707825a32bee
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-07-10 23:57:39 +0100

    Route for product and product/{id}/price

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
+Route::post('/product', [ProductController::class, 'store']);
+Route::put('/product/{id}/price', [ProductController::class, 'updatePrice']);
 });

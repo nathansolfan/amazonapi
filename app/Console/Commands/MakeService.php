1c35cb3b03fea369d5492fc8e47f46c00e1c2c48
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-07-11 00:38:20 +0100

    AMazonDataService

--- app/Console/Commands/MakeService.php
+++ app/Console/Commands/MakeService.php
@@ -3,36 +3,33 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use Illuminate\Contracts\Filesystem\Filesystem;
-use Illuminate\Filesystem\Filesystem as FilesystemFilesystem;
+use Illuminate\Filesystem\Filesystem;
 
 class MakeService extends Command
 {
-    
-    protected $signature = 'app:make-service';
-    protected $description = 'Command description';
+    protected $signature = 'make:service {name}';
+    protected $description = 'Create a new service class';
 
     protected $files;
 
-    public function __construct(FilesystemFilesystem $files)
+    public function __construct(Filesystem $files)
     {
         parent::__construct();
-        $this->files = $files;        
+        $this->files = $files;
     }
 
     public function handle()
     {
-        //
         $name = $this->argument('name');
         $path = app_path("Services/{$name}.php");
-        
-        if($this->files->exists($path)){
-            $this->error("Service {$name} already exist");
+
+        if ($this->files->exists($path)) {
+            $this->error("Service {$name} already exists!");
             return;
         }
 
         $stub = $this->files->get(__DIR__ . '/stubs/service.stub');
-        $stub = str_replace('Dummy Class', $name, $stub);
+        $stub = str_replace('DummyClass', $name, $stub);
 
         $this->files->put($path, $stub);
         $this->info("Service {$name} created successfully.");

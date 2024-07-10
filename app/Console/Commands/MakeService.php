4e28e1a0b6ecdf0638cf93f6ab27b40be679d8a7
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-07-11 00:34:55 +0100

    Make service

--- app/Console/Commands/MakeService.php
+++ app/Console/Commands/MakeService.php
@@ -3,28 +3,38 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Contracts\Filesystem\Filesystem;
+use Illuminate\Filesystem\Filesystem as FilesystemFilesystem;
 
 class MakeService extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
+    
     protected $signature = 'app:make-service';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Command description';
 
-    /**
-     * Execute the console command.
-     */
+    protected $files;
+
+    public function __construct(FilesystemFilesystem $files)
+    {
+        parent::__construct();
+        $this->files = $files;        
+    }
+
     public function handle()
     {
         //
+        $name = $this->argument('name');
+        $path = app_path("Services/{$name}.php");
+        
+        if($this->files->exists($path)){
+            $this->error("Service {$name} already exist");
+            return;
+        }
+
+        $stub = $this->files->get(__DIR__ . '/stubs/service.stub');
+        $stub = str_replace('Dummy Class', $name, $stub);
+
+        $this->files->put($path, $stub);
+        $this->info("Service {$name} created successfully.");
     }
 }
